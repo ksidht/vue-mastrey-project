3d25fed1ae4dc199388738382e97acb14b944ad4
Siddharth <sidkdb@gmail.com>
2022-07-15 17:53:20 +0530

    Project setup

---
+++ src/views/EventView.vue
@@ -0,0 +1,32 @@
+<script> 
+import EventCard from '../components/EventCard.vue';
+
+export default {
+
+
+mounted() {
+  console.log(`the component is now mounted.`, this.$route)
+}
+
+}
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>Event</h1>
+    <div>{{ $route.query.page}}</div>
+    <div>{{ $route.params.page}}</div>
+    <EventCard></EventCard>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
